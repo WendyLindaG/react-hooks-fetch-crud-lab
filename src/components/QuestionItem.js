d6dd388c6f3c00c7b51cc59d03efd90c6fbf827e
Wendy Linda Gakii <wendylinda2017@gmail.com>
2023-05-03 22:42:34 +0300

    completed assignment

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+
+function QuestionItem({ question, deleteQuestion, answerChange}) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -9,17 +10,30 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+
+  function handleDeleteClick() {
+    if (window.confirm("danger zone!Are you sure you want to delete?")===true){
+      deleteQuestion(id);
+    } 
+  
+  }
+
+  function handleChange(e){
+    answerChange(id, parseInt(e.target.value))
+  }
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleChange}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDeleteClick}>Delete Question</button>
     </li>
   );
 }
 
 export default QuestionItem;
+
